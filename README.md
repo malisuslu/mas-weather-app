1909c115e2a5c242ca02ea9f300a6141a339c6fc
Muhammed Ali SUSLU <malisuslu@yandex.com>
2022-09-26 19:10:26 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,74 @@
+<!-- Please update value in the {}  -->
+
+<h1 align="center">Weather App with Django
+</h1>
+
+
+
+<div align="center">
+  <h3>
+    <a href="https://mas-weather-app.herokuapp.com/">
+      Demo
+    </a>
+     | 
+    <a href="https://github.com/malisuslu/mas-weather-app">
+      Project
+    </a>
+ 
+  </h3>
+</div>
+
+<!-- TABLE OF CONTENTS -->
+
+## Table of Contents
+
+- [Table of Contents](#table-of-contents)
+- [Overview](#overview)
+  - [Built With](#built-with)
+- [How To Use](#how-to-use)
+- [Acknowledgements](#acknowledgements)
+- [Contact](#contact)
+
+<!-- OVERVIEW -->
+
+## Overview
+
+![screenshot](todo.PNG)
+
+### Built With
+
+<!-- This section should list any major frameworks that you built your project using. Here are a few examples.-->
+
+- HTML
+- CSS
+- JS
+- Django
+
+## How To Use
+
+<!-- This is an example, please update according to your application -->
+
+To clone and run this application, you'll need [Git](https://git-scm.com) 
+```bash
+# Clone this repository
+$ git clone git@github.com:malisuslu/mas-todo-app.git
+
+# Install dependencies
+    $ python -m venv env
+    > env/Scripts/activate (for win OS)
+    $ source env/bin/activate (for macOs/linux OS)
+    $ pip install -r requirements.txt
+# Edit .backend.env to .env
+# Add SECRET_KEY in .env file
+
+# Run the app
+    $ python manage.py runserver
+```
+
+## Acknowledgements
+- Information for your projects
+
+## Contact
+
+- GitHub [@malisuslu](https://github.com/malisuslu)
+- Linkedin [@malisuslu](https://www.linkedin.com/in/muhammed-ali-s%C3%BCsl%C3%BC/)
---
+++ weatherapp/views.py
@@ -0,0 +1,74 @@
+from django.shortcuts import render
+import requests
+from decouple import config
+from pprint import pprint
+from datetime import datetime
+
+# Create your views here.
+def index(request):
+
+    API_KEY = config('API_KEY')
+    city = request.POST.get('city') or 'Ankara'
+
+    url_week = 'https://api.openweathermap.org/data/2.5/forecast?q={}&appid={}&units=metric'.format(city, API_KEY)
+
+    r_week = requests.get(url_week).json()
+
+    url_today = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric'.format(city, API_KEY)
+
+    r_today = requests.get(url_today).json()
+
+
+    if r_today['cod'] == 200 and r_week:
+
+        week_days = []
+        icons = []
+        temps = []
+        for i in range(0, 40, 8):
+            week_days.append(datetime.fromtimestamp(r_week['list'][i]['dt']).strftime('%a'))
+            icons.append(r_week['list'][i]['weather'][0]['icon'])
+            temps.append(r_week['list'][i]['main']['temp'])
+
+        context = {
+            'city': r_today['name'].title() + ', ' + r_today['sys']['country'],
+            'date': datetime.fromtimestamp(r_today['dt']).strftime('%d %b %y %a'),
+            'desc': r_today['weather'][0]['description'].title(),
+            'temp': r_today['main']['temp'],
+            'feel': r_today['main']['feels_like'],
+            'min': r_today['main']['temp_min'],
+            'max': r_today['main']['temp_max'],
+            'rain': r_today['rain']['1h'] if 'rain' in r_today else 0,
+            'pressure': r_today['main']['pressure'],
+            'humidity': r_today['main']['humidity'],
+            'wind': r_today['wind']['speed'],
+            'icon': r_today['weather'][0]['icon'],
+            'week_days': week_days,
+            'icons': icons,
+            'temps': temps,
+            'lat': r_today['coord']['lat'],
+            'lon': r_today['coord']['lon'],
+        }
+    else:
+        context = {
+            'city': 'Region not found',
+            'date':  datetime.now().strftime('%d %b %y %a'),
+            'desc': '',
+            'temp': '',
+            'feel': '',
+            'min': '',
+            'max': '',
+            'pressure': '',
+            'humidity': '',
+            'wind': '',
+            'icon': '',
+            'country': '',
+            'sunrise': '',
+            'sunset': '',
+            'week_days': '',
+            'icons': [ 'No-img' for i in range(5)],
+            'temps': '',
+            'lat': '',
+            'lon': '',
+        }
+
+    return render(request, 'weatherapp/index.html', context)
