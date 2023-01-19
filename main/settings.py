f7d332c7852563f104fe0743f7439bb45adc1edf
Muhammed Ali SUSLU <malisuslu@yandex.com>
2023-01-19 11:55:44 +0000

    update settings.py

--- main/settings.py
+++ main/settings.py
@@ -28,7 +28,7 @@ API_KEY = config('API_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', '.vercel.app']
 
 
 # Application definition
